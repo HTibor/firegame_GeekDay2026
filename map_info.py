ee158f0aa80b0ec5adfe9bb3393811cb7a2a0aae
Ben <tothbenedek04@gmail.com>
2026-04-24 23:44:48 +0200

    sex2.0

---
+++ map_info.py
@@ -0,0 +1,25 @@
+class map_info:
+    def __init__(self):
+        # Key: (x, y) tuple
+        # Value: Fire HP (int)
+        self.fires = {}
+
+        # Key: (x, y) tuple
+        # Value: Water amount available (int) or True if infinite
+        self.water_sources = {}
+
+        self.obsticles = {}
+
+        self.units = {}
+
+    def update_fires(self, x, y, hp):
+        self.fires[(x, y)] = hp
+
+    def update_waters(self, x, y, value):
+        self.water_sources[(x, y)] = value
+
+    def update_obsticles(self, x, y):
+        self.obsticles[(x, y)]
+
+    def update_units(self, x, y, id):
+        self.units[id] = (x, y)
